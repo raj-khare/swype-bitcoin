7b16ca5eb0d8f701d8f4affd3781b86d1f009f9d
Raj Khare <seven.triangles@icloud.com>
2022-03-23 00:39:00 +0530

    Initial Commit

---
+++ swype-bitcoin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swype-bitcoin
+//
+//  Created by Raj Khare on 3/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swype-bitcoin/swype_bitcoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  swype_bitcoinApp.swift
+//  swype-bitcoin
+//
+//  Created by Raj Khare on 3/23/22.
+//
+
+import SwiftUI
+
+@main
+struct swype_bitcoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
